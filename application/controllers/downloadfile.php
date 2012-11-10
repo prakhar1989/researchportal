c700d67a2d001a7c1566249215d3dc4055790acd
Anurag Negi <imlucky.23@gmail.com>
2012-11-10 23:31:16 +0530

    Added this file for addind the download file link

---
+++ application/controllers/downloadfile.php
@@ -0,0 +1,46 @@
+<?php
+// block any attempt to the filesystem
+/*if (isset($_GET['file']) && basename($_GET['file']) == $_GET['file']) {
+    
+} else {
+    $filename = NULL;
+}
+*/
+class Downloadfile extends CI_Controller {
+function index ()
+{
+	$filename = $_GET['file'];
+
+	$err = '<p style="color:#990000">Sorry, the file you are requesting is unavailable.</p>';
+	echo 'File is '.$filename;
+
+	if (!$filename) {
+			// if variable $filename is NULL or false display the message
+	//echo $err;
+		} else {
+			// define the path to your download folder plus assign the file name
+			$path = 'upload/'.$filename;
+			// check that file exists and is readable
+			if (file_exists($path) && is_readable($path)) {
+				// get the file size and send the http headers
+				$size = filesize($path);
+				header('Content-Type: application/octet-stream');
+				header('Content-Length: '.$size);
+				header('Content-Disposition: attachment; filename='.$filename);
+				header('Content-Transfer-Encoding: binary');
+				// open the file in binary read-only mode
+				// display the error messages if the file can´t be opened
+				$file = @ fopen($path, 'rb');
+				if ($file) {
+					// stream the file and exit the script when complete
+					fpassthru($file);
+					exit;
+				} else {
+					//echo $err;
+				}
+			} else {
+				//echo $err;
+			}
+	}	
+	}	
+	}
