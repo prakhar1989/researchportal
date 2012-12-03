7237e1dc82c341f885a5ba0998522c3001296e0b
unknown <vridhic2014@email.iimcal.ac.in>
2012-12-04 00:43:26 +0530

    extension_chairman

--- application/controllers/extension_chairman.php
+++ application/controllers/extension_chairman.php
@@ -20,7 +20,7 @@ class extension_chairman extends CI_Controller
 				echo '<FORM METHOD=POST ACTION="ExtensionCheckChairmanRequest">
 				<TABLE width="90%" border="1" bordercolor="#993300" align="center" cellpadding="3" cellspacing="1" class="table_border_both_left"><tr  class="heading_table_top"> 
 					 <table class="table table-bordered">
-					<tr><TD><h4>ProjectTitle</h4></TD><TD><h4>ProjectId</h4></TD><TD><h4>Description</h4></TD><TD><h4>ProjectCategory</h4></TD><TD><h4>ProjectGrant</h4><TD><h4>App_Date</h4></TD><TD><h4>Researcher1</h4></TD><TD><h4>Researcher2</TD><TD><h4>Researcher3</TD><TD><h4>Select</h1></TD></tr>
+					<tr><TD><h4>ProjectTitle</h4></TD><TD><h4>ProjectId</h4></TD><TD><h4>Description</h4></TD><TD><h4>ProjectCategory</h4></TD><TD><h4>ProjectGrant</h4><TD><h4>App_Date</h4></TD><TD><h4>Researcher1</h4></TD><TD><h4>Researcher2</TD><TD><h4>Researcher3</TD><TD><h4>Comments</h4></TD><TD><h4>Select</h1></TD></tr>
 					
 					<tbody>';
 					 foreach($Query->result() as $row)
@@ -43,6 +43,7 @@ class extension_chairman extends CI_Controller
 						 print $row->Researcher2;
 						 echo '</TD><TD>';
 						 print $row->Researcher3;
+					         echo '</TD><TD>';
 						  //vridhi--display comments below
 						 $Result=$this->project_model->getComment($row->ProjectId, $_SESSION['usertype']);
 						 foreach($Result as $row1)
