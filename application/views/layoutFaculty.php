1658b08e59b6a08690894d51763dba68857297c8
unknown <vridhic2014@email.iimcal.ac.in>
2012-12-17 17:59:35 +0530

    pending tab for faculty

--- application/views/layoutFaculty.php
+++ application/views/layoutFaculty.php
@@ -59,6 +59,8 @@ IIIIIIIIII SSSSSSSSSSSSSSS           GGGGGG   GGGG
                         <li><a  href="/rp/FacultyProjOngoing">View Ongoing Projects</a> </li>
 						<li><a  href="/rp/FacultyProjApp">Apply for Project</a> </li>
 						<li><a  href="/rp/FacultyProjCompleted">View Completed Projects</a> </li>
+						<li><a  href="FacultyProjPending">View Pending Projects</a> </li>
+
                     </ul>
                 </div>
                 <div id="confs" style="display: none">
