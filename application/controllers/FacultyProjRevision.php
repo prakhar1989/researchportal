d6873368852f3232e2197ab840238e8cb84493ba
unknown <vridhic2014@email.iimcal.ac.in>
2012-12-19 02:00:31 +0530

    revision tab

---
+++ application/controllers/FacultyProjRevision.php
@@ -0,0 +1,65 @@
+<?PHP
+
+class FacultyProjRevision extends CI_Controller {
+	
+	function index()
+		{
+			
+		$data['myClass']=$this;
+		$data['action']=0;
+		session_start();
+		if($_SESSION['usertype']==4){
+		$this->load->view('layoutFaculty',$data);
+		} 
+		else{
+		header("location:login");
+		}
+}
+	function load_php()
+				{
+				$ProjectID = $this->input->post('ProjectChoice');
+				//echo $ProjectID;
+				//Load the project model
+				$this->load->model('project_model');
+				$result= $this->project_model->projectRevise($ProjectID);
+				//echo '<p> hello this is the Project Details Page </p>';
+				// Display the results
+				echo'
+					<FORM METHOD=POST ACTION="http://localhost/rp/FacultyReviseApp">
+					<table class="table table-bordered">
+					<tr<><td></td></tr>
+					<TD><h4>ProjectTitle</h4></TD><TD><h4>ProjectId</h4></TD><TD><h4>Start Date</h4></TD><TD><h4>End Date</h4></TD><TD><h4>Researcher1</h4></TD><TD><h4>Researcher2</TD><TD><h4>Researcher3</TD><TD><h4></h1></TD>
+					
+					<tbody>	
+					<INPUT TYPE="HIDDEN" NAME="ProjectSelected" VALUE="'.$ProjectID.'">					
+					';
+					foreach($result->result() as $row)
+						{
+						echo '<TR><TD>';
+						 print $row->ProjectTitle;
+						 echo '</TD><TD>';
+						 print $row->ProjectId;
+						 echo '</TD><TD>';
+						 print $row->Start_Date;
+						 echo '</TD><TD>';
+						 print $row->End_Date;
+						 echo '</TD><TD>';
+						 print $row->Researcher1;
+						 echo '</TD><TD>';
+						 print $row->Researcher2;
+						 echo '</TD><TD>';
+						 print $row->Researcher3;
+						 echo '</TD>';
+						 echo '<TD><INPUT TYPE="RADIO" NAME="ProjectChoice" VALUE="'.$row->ProjectId.'"></TD></TR>';
+						}			 
+					 echo '</tbody></TABLE>
+					<INPUT TYPE=SUBMIT name ="RequestType" value="Edit">
+					</FORM>';
+                
+				
+				}
+	
+	}
+
+
+?>
