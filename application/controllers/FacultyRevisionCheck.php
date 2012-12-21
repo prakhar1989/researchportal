eb24def7b0e3a6517c3f804a117086d8b37a7a87
Anurag Negi <imlucky.23@gmail.com>
2012-12-21 19:28:21 +0530

    new files added

---
+++ application/controllers/FacultyRevisionCheck.php
@@ -0,0 +1,46 @@
+<?PHP
+
+class FacultyRevisionCheck extends CI_Controller 
+{
+	function index()
+		{			
+		$data['myClass']=$this;
+		$data['action']=0;
+		$this->load->view('layoutfaculty',$data);
+		}
+	function load_php()
+				{
+				$this->load->model('project_model');
+				session_start();
+				$_SESSION['ProjectID'] = $_POST['ProjectSelected'];
+				echo'
+					<FORM METHOD=POST ACTION="#">
+					<TABLE width="90%" border="1" bordercolor="#993300" align="center" cellpadding="3" cellspacing="1" class="table_border_both_left"><tr  class="heading_table_top"> 
+							';
+					if ($_POST['RequestType'] == 'Edit New Application')
+						{
+						//$this->project_model->projectCompletionChairmanResponse('Approve',$ProjectID);
+						//$this->project_model->insertComment($_SESSION['username'], $_SESSION['usertype'], $ProjectID, addslashes(trim($_POST['comment'])), "chairman_approve_completion");
+						header("Location: /rp/FacultyReviseApp");
+						} 
+					else if ($_POST['RequestType'] == 'Edit Extension') 
+						{
+						//$this->project_model->projectCompletionChairmanResponse('Reject',$ProjectID);
+						//$this->project_model->insertComment($_SESSION['username'], $_SESSION['usertype'], $ProjectID, addslashes(trim($_POST['comment'])), "chairman_reject_completion");
+						header("Location: /rp/FacultyRevExt");
+						}
+					else if ($_POST['RequestType'] == 'Edit Completion') 
+						{
+						//$this->project_model->projectCompletionChairmanResponse('Send For Revision',$ProjectID);
+						//$this->project_model->insertComment($_SESSION['username'], $_SESSION['usertype'], $ProjectID, addslashes(trim($_POST['comment'])), "chairman_consult_extension");
+						//header("Location: /rp/extension_chairman");
+						}
+					echo "\n\n";
+					 echo '</TABLE>
+					</FORM>';
+                
+				
+				}
+	
+	}
+?>
