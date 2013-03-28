c77437ef88d7f8c6ca5504d2ad64ddd480396d4f
unknown <nikhill2014@email.iimcal.ac.in>
2013-03-28 13:26:59 +0530

    hj

--- application/controllers/CompletionCheckAdminRequest.php
+++ application/controllers/CompletionCheckAdminRequest.php
@@ -7,12 +7,17 @@ class CompletionCheckAdminRequest extends CI_Controller
 		$data['myClass']=$this;
 		$data['action']=0;
 		session_start();
-		if($_SESSION['usertype']==1){
-		$this->load->view('layout',$data);
-		} 
-		else{
-		header("location:login");
-		}
+			if($_SESSION['usertype']==1){
+				$this->load->view('layout',$data);
+			} elseif ($_SESSION['usertype']==2){
+				$this->load->view('layoutComm',$data);
+			} elseif($_SESSION['usertype']==3){
+				$this->load->view('layoutChairman',$data);
+			}
+			else{
+			
+			header("location:login");
+			}
 		}
 	function load_php()
 				{
