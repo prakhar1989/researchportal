dc96e96c5be03fbc29c304b26e8a2225d61305d3
unknown <vridhic2014@email.iimcal.ac.in>
2013-08-09 15:21:15 +0530

    buggy conf# Please enter the commit message for your changes. Lines starting

--- application/models/conference_model.php
+++ application/models/conference_model.php
@@ -245,9 +245,16 @@ class Conference_model extends CI_Model {
 		 	$queryStr= 'INSERT INTO conference (ConferenceTitle , Description , Start_Date, End_Date, Researcher1 , ConferenceCategory , ConferenceGrant , CStatus) VALUES (\''.$data['title'].'\' , \'' .$data['desc'].'\' , \''.$data['start_date'].'\' , \''.$data['end_date'].'\' , \''.$user.'\' , \''.$data['category'].'\' , \''.$data['grant'].'\' , \'app_admin\' );' ;
 		 	//echo '<br>'.$queryStr;
 		 	$query = $this->db->query($queryStr);
-		 	//$result = $query->result();
-		 	$msg='The Conference has been Sent for approval';
-		 	return $msg;
+			$queryStr1 = 'SELECT ConferenceId FROM conference WHERE ConferenceId = (SELECT MAX(ConferenceId)  FROM conference)';
+			$query1 = $this->db->query($queryStr1);
+			$result1=$query1->result();
+			foreach($result1 as $row)
+						{
+							 $ConfId=$row->ConfId;
+						}
+			
+			return $ConfId;
+		 	
 		 }
 		 // function to get the account status of the project--vridhi
 		 function getC_AccStatus ($conferenceId)
