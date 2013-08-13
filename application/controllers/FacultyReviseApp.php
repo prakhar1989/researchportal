8f55e86e98d002da3966fc0903abfcac78ec1ad9
unknown <vridhic2014@email.iimcal.ac.in>
2013-08-13 18:48:03 +0530

    sdjbchbs

--- application/controllers/FacultyReviseApp.php
+++ application/controllers/FacultyReviseApp.php
@@ -270,3 +270,10 @@ function insert()
 
 
 ?>
+<script>
+function enableMe(fld)
+{
+	eval('window.document.application.'+fld+'.disabled = !window.document.application.'+fld+'CB'+'.checked');
+
+}
+</script>
