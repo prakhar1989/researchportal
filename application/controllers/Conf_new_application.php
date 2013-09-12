b01aa9a0e927ddb0dab7b161ba6f18614622db6f
unknown <nikhill2014@email.iimcal.ac.in>
2013-09-13 01:53:55 +0530

    fas

--- application/controllers/Conf_new_application.php
+++ application/controllers/Conf_new_application.php
@@ -29,7 +29,7 @@ class Conf_new_application extends CI_Controller {
 					 <FORM METHOD=POST ACTION="Conf_show">
                      <table class="table table-bordered">
  					
-					<TR><TD><h4>Conference Title</h4></TD><TD><h4>Application Date</h4></TD><TD><h4>Faculty Name</h4></TD><TD><h4>Faculty Category</h4></TD><TD><h4>Co Author (if any)</h4></TD><TD><h4>Select</h4></TD></TR>
+					<TR><TD><h4>Faculty Name</h4></TD><TD><h4>Conference Title</h4></TD><TD><h4>Application Date</h4></TD><TD><h4>Co Author (if any)</h4></TD><TD><h4>Faculty Category</h4></TD><TD><h4>Select</h4></TD></TR>
                      ';
 					 $flag=0;
 					 foreach($data['query'] as $row)
@@ -50,16 +50,16 @@ class Conf_new_application extends CI_Controller {
 						 
 						 $flag++;
 						 echo '<TR><TD>';
+						 print $row->Researcher1;
+						 echo '</TD><TD>';
 						 print $row->ConferenceTitle;
 						 echo '</TD><TD>';
 						 print $row->App_Date;
 						 echo '</TD><TD>';
-						 print $row->Researcher1;
-						 echo '</TD><TD>';
-						 print $row->FacultyCategory;
-						 echo '</TD><TD>';
 						 print $row->Researcher2;
-					     echo '</TD><TD><INPUT TYPE="RADIO" NAME="Choice1" VALUE="'.$row->ConferenceId.'"></TD></TR>';
+					     echo '</TD><TD>';
+						 print $row->FacultyCategory;
+						 echo '</TD><TD><INPUT TYPE="RADIO" NAME="Choice1" VALUE="'.$row->ConferenceId.'"></TD></TR>';
 					 }
 					 if($flag==0){
 					 echo '<h4>No New Applications</h4> <br > </TABLE>';
